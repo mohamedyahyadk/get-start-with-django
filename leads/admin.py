3b881098765db66859c1fb1814d1dad9a37c0e1b
Mohamed Yahya Dkhila <23063@supnum.mr>
2025-03-23 23:12:24 +0000

    second commit: first app ,models,view,template,forms,modelform

---
+++ leads/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import User ,Lead,Agent
+
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
+
+
+
---
+++ leads/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import lead_list ,lead_detail ,lead_create ,lead_update,lead_delete
+
+urlpatterns=[
+     path('', lead_list ),
+     path('create/', lead_create),
+     path('<int:pk>/update/', lead_update),
+     path('<int:pk>/delete/', lead_delete),
+     path('<int:pk>/', lead_detail),
+    
+]
