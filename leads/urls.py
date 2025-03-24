dca8bfa26c6731ec4b717778e1f734f1f8da0426
Mohamed Yahya Dkhila <23063@supnum.mr>
2025-03-24 21:01:58 +0000

    fourth commit: adding clase based views instead of functions based views

--- leads/urls.py
+++ leads/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import lead_list ,lead_detail ,lead_create ,lead_update,lead_delete
+from .views import lead_list ,lead_detail ,lead_create ,lead_update,lead_delete ,LeadListView ,LeadDetailtView,LeadCreateView,LeadUpdateView,LeadDeleteView
 app_name = "leads"  # ✅ This registers the namespace
 urlpatterns=[
-     path('', lead_list ,name='lead-list'),
-     path('<int:pk>/', lead_detail ,name='lead-detail'),
-     path('create/', lead_create ,name='lead-create'),
-     path('<int:pk>/update/', lead_update ,name='lead-update'),
-     path('<int:pk>/delete/', lead_delete ,name='lead-delete'),
+     path('', LeadListView.as_view() ,name='lead-list'),
+     path('<int:pk>/', LeadDetailtView.as_view() ,name='lead-detail'),
+     path('create/', LeadCreateView.as_view() ,name='lead-create'),
+     path('<int:pk>/update/', LeadUpdateView.as_view() ,name='lead-update'),
+     path('<int:pk>/delete/', LeadDeleteView.as_view() ,name='lead-delete'),
     
 ]
--- leads/views.py
+++ leads/views.py
@@ -4,12 +4,18 @@ from leads.models import Lead ,Agent
 from .forms import LeadForm ,LeadModelForm
 # Create your views here.
 
+from django.views.generic import TemplateView  ,ListView ,DetailView ,CreateView ,UpdateView ,DeleteView
+class LandingPageView(TemplateView):
+    template_name='landing.html'
 
 def landing_page(request):
       return render(request,'landing.html')
 
 
-
+class  LeadListView(ListView):
+     template_name="leads/lead_list.html"
+     queryset=Lead.objects.all()
+     context_object_name='leads'
 
 def lead_list(request):
   leads=Lead.objects.all()
@@ -18,6 +24,14 @@ def lead_list(request):
   }
   return render(request,"leads/lead_list.html",context)
 
+
+
+class  LeadDetailtView(DetailView):
+     template_name="leads/lead_detail.html"
+     queryset=Lead.objects.all()
+     context_object_name='lead'
+
+
 def lead_detail(request,pk):
 
     lead=Lead.objects.get(id=pk)
@@ -25,6 +39,13 @@ def lead_detail(request,pk):
     "lead":lead
   }
     return render(request,"leads/lead_detail.html",context)
+
+
+
+class  LeadCreateView(CreateView):
+     template_name="leads/lead_create.html"
+     form_class=LeadModelForm
+     success_url='/leads'
 
 def lead_create(request):
    form=LeadModelForm()
@@ -40,6 +61,13 @@ def lead_create(request):
    return render(request,"leads/lead_create.html",context)
 
 
+
+class  LeadUpdateView(UpdateView):
+     template_name="leads/lead_update.html"
+     queryset=Lead.objects.all()
+     form_class=LeadModelForm
+     success_url='/leads'
+
 def  lead_update(request ,pk):
    lead=Lead.objects.get(id=pk)
    form=LeadModelForm(instance=lead)
@@ -54,6 +82,11 @@ def  lead_update(request ,pk):
    }
    return render(request,"leads/lead_update.html",context)
 
+
+class  LeadDeleteView(DeleteView):
+     template_name="leads/lead_delete.html"
+     queryset=Lead.objects.all()
+     success_url='/leads'
 
 def lead_delete(request,pk):
    lead=Lead.objects.get(id=pk)
