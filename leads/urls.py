6bd7b6e40dbdaed2e66c5f695cc1f1134e478586
Mohamed Yahya Dkhila <23063@supnum.mr>
2025-03-24 08:06:28 +0000

    third commit : tailwaindcss

--- leads/urls.py
+++ leads/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import lead_list ,lead_detail ,lead_create ,lead_update,lead_delete
-
+app_name = "leads"  # ✅ This registers the namespace
 urlpatterns=[
-     path('', lead_list ),
-     path('create/', lead_create),
-     path('<int:pk>/update/', lead_update),
-     path('<int:pk>/delete/', lead_delete),
-     path('<int:pk>/', lead_detail),
+     path('', lead_list ,name='lead-list'),
+     path('<int:pk>/', lead_detail ,name='lead-detail'),
+     path('create/', lead_create ,name='lead-create'),
+     path('<int:pk>/update/', lead_update ,name='lead-update'),
+     path('<int:pk>/delete/', lead_delete ,name='lead-delete'),
     
 ]
