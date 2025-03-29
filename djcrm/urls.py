ce41cb3b8dd7402a011295282a04a3848c4f6401
Mohamed Yahya Dkhila <23063@supnum.mr>
2025-03-29 00:33:56 +0000

    (practical):commit 8 : password reset

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -3,13 +3,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path ,include
 from leads.views import  landing_page ,LandingPageView ,SignupView
-from django.contrib.auth.views import LoginView ,LogoutView 
+from django.contrib.auth.views import (
+     LoginView ,
+     LogoutView  , 
+     PasswordResetView ,
+     PasswordResetDoneView ,
+     PasswordResetConfirmView,
+     PasswordResetCompleteView
+     )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view() ,name='landing_page'),
     path('leads/',include("leads.urls")),
     path('agents/',include("agents.urls")),
     path('signup/',SignupView.as_view(),name='signup'),
+    path('reset-password/',PasswordResetView.as_view(),name='reset-password'),
+    path('password-reset-done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('login/',LoginView.as_view(),name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
    
